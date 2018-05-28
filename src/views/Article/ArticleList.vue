25f81293d10bbe0ff6972528a281c2f436ef06ef
Bener <2388697970@qq.com>
2018-05-28 18:42:59 +0800

    init article

---
+++ src/views/Article/ArticleList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="artical-wrap">
+    this is artical list container
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+
+</script>
+
+<style scoped lang="styl">
+  .artical-wrap
+    height 200px
+    border 1px solid green
+</style>
+
