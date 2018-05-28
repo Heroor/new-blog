e654377574593731502acde2dcbd70c84d377da1
Bener <2388697970@qq.com>
2018-05-28 18:43:25 +0800

    init header banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,38 @@
+<template>
+  <div :class="{
+      'banner-wrap': true,
+      'hidden': !bannerSrc
+    }">
+    <div class="banner-container position--align">
+      <img :src="bannerSrc" ondragstart="return false" class="transition-05">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        bannerSrc: '123',
+        title: 'title',
+        desc: 'desc'
+      }
+    }
+  }
+
+</script>
+
+<style scoped lang="styl">
+  .banner-wrap
+    position relative
+    top -20px
+    height 18px
+    &.hidden
+      overflow hidden
+    .banner-container
+      height 420px
+      min-height 18px
+      width 280%
+      border-radius 50% 50% 0 0
+      background #eef7f7
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header-wrap">
+    <Nav />
+    <Banner />
+  </div>
+</template>
+
+<script>
+  import Banner from '@/components/Banner'
+  import Nav from '@/components/Nav'
+
+  export default {
+    components: {
+      Banner,
+      Nav
+    }
+  }
+
+</script>
+
+<style scoped lang="styl">
+
+
+</style>
