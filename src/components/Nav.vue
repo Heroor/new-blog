95127f0ff719993c8180f6a0e8484dfaa2f955ae
Bener <2388697970@qq.com>
2018-05-28 18:42:27 +0800

    init nav

---
+++ src/components/Nav.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="nav-wrap">
+    <ul class="nav-ul">
+      <li v-for="(v, i) in navList" :key="i">
+        <router-link :to="v.link">
+          {{v.name}}
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        navList: [{
+          name: 'Home',
+          link: '/'
+        }, {
+          name: 'Info',
+          link: '/info'
+        }]
+      }
+    }
+  }
+
+</script>
+
+<style scoped lang="styl">
+  .nav-ul
+    height 50px
+    text-align center
+    padding-bottom 18px
+    font-size 15px
+    background #89d4a7
+    li
+      display inline-block
+      margin 0 12px
+      a
+        line-height 48px
+        display inline-block
+        padding 0 8px
+        text-decoration none
+        color #fff
+</style>
