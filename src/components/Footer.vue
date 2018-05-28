8367d4887517253931b53f6d272191096e2368f7
Bener <2388697970@qq.com>
2018-05-28 18:43:32 +0800

    init footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="footer-wrap">
+    this is footer container
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+
+</script>
+
+<style scoped lang="styl">
+  .footer-wrap
+    border 1px solid red
+    margin-top 10px
+</style>
+
