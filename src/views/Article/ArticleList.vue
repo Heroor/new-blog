152166b076b767bb5059a8f9c62959c75357f414
Bener <2388697970@qq.com>
2018-06-02 22:28:16 +0800

    feat: 调整首页文章列表样式

--- src/views/Article/ArticleList.vue
+++ src/views/Article/ArticleList.vue
@@ -7,9 +7,21 @@
         default() {
           return [{
             title: 'one',
-            poster: '123',
+            poster: 'https://blog.benjavan.top/static/images/avatar.jpg',
             desc: 'this is a article',
+            date: '2018-12-12',
             id: '1'
+          }, {
+            title: 'two',
+            desc: 'this is a article too',
+            date: '2018-12-31',
+            id: '2'
+          }, {
+            title: '中文',
+            poster: 'https://blog.benjavan.top/static/images/avatar.jpg',
+            desc: 'this is a article too',
+            date: '2018-12-33',
+            id: '3'
           }]
         }
       }
@@ -22,12 +34,20 @@
             {
               articleList.map((v, i) =>
                 <li key={i}>
-                  <router-link to={v.id} class="article__title">
-                    <h2 class="w-b__b-a">{v.title}</h2>
-                  </router-link>
+                  <h2 class="w-b--b-a">
+                    <router-link to={'article/' + v.id} class="article__title">{v.title}</router-link>
+                  </h2>
                   <div class="article__content">
-                    <p class="article__desc">{v.desc}</p>
-                    <img class="article__img" src={v.poster}/>
+                    <div class="article__left">
+                      <p class="article__desc w-b--b-a">{v.desc}</p>
+                      <span class="article__date">{v.date}</span>
+                    </div>
+                    {
+                      v.poster &&
+                      <div class="article__right">
+                        <img class="article__img" src={v.poster} alt=""/>
+                      </div>
+                    }
                   </div>
                 </li>
               )
@@ -51,18 +71,47 @@
     li
       margin-top 25px
       padding 30px
-      border-radius 5px
+      border-radius 6px
       background-color #fff
-      transition all .3s
-      .article__title
-        color $font-color-title
-      .article__content
-        display flex
-        .article__desc
-          flex 1
-          color $font-color-content
-        .article__img
-          width 180px
-          height 180px
+      transition(.3s)
+      &:hover
+        box-shadow 0 0 30px rgba(35, 180, 226, 0.2)
+      h2
+        text-align center
+        margin-bottom 30px
+  .article__title
+    display inline-block
+    font-size $font-title
+    transition(.4s)
+    color $font-color-title
+    &:hover
+      color $nav-bg
+  .article__content
+    display flex
+    min-height 80px
+  .article__left
+    display flex
+    flex 1
+    flex-direction column
+    transition(.4s)
+  .article__desc
+    flex 1
+    margin-bottom 20px
+    font-size $font-content
+    color $font-color-content
+  .article__date
+    font-size $font-tips
+    color #858585
+  .article__right
+    margin-left 20px
+  .article__img
+    transition(.4s)
+    display block
+    max-width 180px
+    max-height 180px
+    border-radius 10px
+    // @media screen and (max-width 760px){
+      // width 20px
+    // }
 </style>
 
