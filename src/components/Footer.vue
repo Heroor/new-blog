ec8a1861f8f1964edf5fe12acfa6c82ffee3eca0
Bener <2388697970@qq.com>
2018-06-02 23:16:09 +0800

    feat: 添加footer组件样式

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="footer-wrap">
-    <p class="hosted t-a-c" style="line-height: 4">
+    <p class="footer__hosted">
       <span>Hosted by</span>
-      <a href="https://pages.coding.me" style="font-weight: bold;">Coding Pages</a>
+      <a class="footer__strong" href="https://pages.coding.me">Coding Pages</a>
+      <span> | Made by </span>
+      <span class="footer__strong">Bener</span>
     </p>
   </div>
 </template>
@@ -16,7 +18,16 @@
 </script>
 
 <style scoped lang="stylus">
+  @import "../style/mix.styl"
   .footer-wrap
-    margin-top 10px
+    margin-top 20px
+  .footer__hosted
+    line-height 3
+    text-align center
+    color #999
+ .footer__strong
+    font-weight: bold
+    color #587f8c
+
 </style>
 
